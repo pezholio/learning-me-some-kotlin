02b3818d02b40fdf09696919d16332da90f0741b
pezholio <pezholio@gmail.com>
2022-07-05 15:05:39 +0100

    Add Gradle dependencies
    This allows us to create database models via JPA, connect to the
    database, and run migrations via flyway.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.3.2"
   kotlin("plugin.spring") version "1.6.21"
+  kotlin("plugin.jpa") version "1.7.0"
 }
 
 configurations {
@@ -9,6 +10,10 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+  runtimeOnly("org.postgresql:postgresql:42.4.0")
+  runtimeOnly("org.flywaydb:flyway-core:8.5.13")
 }
 
 java {
